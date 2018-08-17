a91f3ce122bddeb6d2792f17f82ee2fccabc9140
Gabriel Berthling-Hansen <gabrielbhansen@hotmail.com>
2018-08-17 21:51:02 +0200

    Bump version number

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 let port = 8000;
+let version = 1;
+
 
 app.get("/", (req, res) => {
-	res.send("Up and runnning");
+	res.send("Up and runnning: " + version);
 })
 
 app.listen(port, () => {
